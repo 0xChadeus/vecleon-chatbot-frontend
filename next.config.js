c13520dc65a91b26256c69205b537f47bea51f29
saze@protonmail.com <saze@protonmail.com>
2024-02-11 19:09:50 +1100

    add path source

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {  webpack: config => {
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
