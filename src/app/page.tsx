7a3ed9d5194f87c215670c15cabd4c1bd77e6bca
saze@protonmail.com <saze@protonmail.com>
2024-03-12 08:54:27 +1100

    escape characters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,13 +35,13 @@ export default function Home() {
         <div className=" col-md-10 col-md-offset-1 section-title">
           <h2 className="relative flex left-0 text-3xl font-sans top-12 md:top-20 font-extrabold">YOUR VERY OWN AI COMPANION, ASSISTANT, FAVORITE CHARACTER, AND TUTOR. UNCENSORED. ALL IN ONE PLACE.</h2>
           <a href="https://vecleon.com/auth/register">
-          <h3 className="relative flex left-0 text-2xl font-sans font-bold top-28 sm:top-40">Valerie's Storybook</h3>
+          <h3 className="relative flex left-0 text-2xl font-sans font-bold top-28 sm:top-40">Valerie&apos;s Storybook</h3>
           <h4 className="relative flex left-0 text-md font-sans top-28 sm:top-40"> Chat with your favorite characters, original and otherwise.</h4>
           <Button className='relative flex top-28 sm:top-44 font-extrabold text-lg' size={"lg"}> GET STARTED </Button>
           </a>
           <h3 className="relative flex left-0 text-2xl font-sans font-bold top-32 sm:top-48">Vecleon Assistant</h3>
           <h4 className="relative flex left-0 text-md font-sans top-32 sm:top-48"> Get advice and tutoring from state-of-the-art assistant models.</h4>
-          <Button className='relative flex top-32 sm:top-52 font-extrabold text-lg' size={"lg"} variant={'outline'}> GET STARTED</Button>
+          <Button className='relative flex top-32 sm:top-52 font-extrabold text-lg' size={"lg"} variant={'outline'}> COMING SOON</Button>
         </div>
         </div>
       </div>
