8f37838df8474c7fb0f637759d8ce18523814c34
saze@protonmail.com <saze@protonmail.com>
2024-04-04 16:19:32 +1100

    fix account page displays

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -54,23 +54,24 @@ const AccountPage = ({}) => {
       url: `${process.env.NEXT_PUBLIC_MIDSERVER_URL}/api/get_subscription/`,
     }).then(function ( response: any) {
         console.log(response);
-        if(response.data[0] == "response: not_subscribed") {
+        if(response.data.response == "not_subscribed") {
           setSubscriptionStatus(false);
-        }      
-        setSubscriptionStatus(true);
-        setSubscriptionCancelled(response.data.cancelled)
-
-        console.log(response.data.subscription);
-        var a = new Date(response.data.subscription.current_period_end * 1000);
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        var year = a.getFullYear();
-        var month = months[a.getMonth()];
-        var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
-        var time = date + ' ' + month + ' ' + year; // + ' ' + hour + ':' + min + ':' + sec ;
-        setCurrentPeriodEnd(time);
+        } else {
+          setSubscriptionStatus(true);
+          setSubscriptionCancelled(response.data.cancelled)
+  
+          console.log(response.data.subscription);
+          var a = new Date(response.data.subscription.current_period_end * 1000);
+          var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+          var year = a.getFullYear();
+          var month = months[a.getMonth()];
+          var date = a.getDate();
+          var hour = a.getHours();
+          var min = a.getMinutes();
+          var sec = a.getSeconds();
+          var time = date + ' ' + month + ' ' + year; // + ' ' + hour + ':' + min + ':' + sec ;
+          setCurrentPeriodEnd(time);  
+        }
       });
   }, [])
 
