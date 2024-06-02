9e0f6de00c3f4806d71642b870912575e88d9c88
chadeus <saze@protonmail.com>
2024-06-02 16:34:04 +1000

    change frontpage aesthetics + image stuff

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,27 +1,13 @@
 'use client'
-import Image from 'next/image'
 import { LandingNavbar } from '@/components/landing-navbar'
-import React, {useEffect, useRef} from 'react';    
-import {useScroll, motion, useTransform} from 'framer-motion';
-import { Button } from '@/components/ui/button';
-import VecleonLogo from '@/components/logo';
+import React from 'react';    
 import Footer from '@/components/footer';
+import Image from 'next/image'
 
 
 
 //home (landing) page
 export default function Info() {
-
-  const container = useRef(null);
-  const {scrollYProgress} = useScroll({
-    target: container,
-    offset: ['start start', 'end start'],
-  });
-
-  const titleScroll = useTransform(scrollYProgress, [0, 1], [0, 700]);
-  const bgScroll = useTransform(scrollYProgress, [0, 1], [-100, -200]);
-  const infoScroll = useTransform(scrollYProgress, [0, 1], [0, -400]);
-  const rd = Math.floor(Math.random() * -75) - 25;
 
   return (
     <>
@@ -31,9 +17,12 @@ export default function Info() {
       <div id="features" className="text-left">
       <div className="container flex flex-col">
         <div className=" col-md-10 col-md-offset-1 section-title top-34">
-          <h1 className="relative flex left-0 text-3xl font-sans top-28 md:top-20 font-extrabold">MAKING AI SYSTEMS THAT WILL IMPROVE YOUR QUALITY OF LIFE</h1>
+          <h1 className="relative flex left-0 text-3xl font-sans top-28 md:top-20 font-extrabold">TALK TO ANYONE, FROM ANY WORLD, ANYTIME, ANYWHERE, FOR ANY REASON.</h1>
           <h2 className="relative flex left-0 text-2xl font-sans font-bold top-28 sm:top-40"></h2>
-          <h2 className="relative flex left-0 text-md font-sans top-28 sm:top-40"> At Vecleon, our mission is to create artificial intelligence systems which  </h2>
+          <h2 className="relative flex left-0 text-md font-sans top-28 sm:top-40"> At Vecleon, our goal is to create a safe and easy place for anyone to make connections with AI companions and characters. It is our belief that AI represents a new frontier of human development, both in terms of economic prosperity and more fundamentally, the way people live. As such, this web application is a stepping stone on the way to realizing this frontier, our hope being that it will serve as a proof of concept for human connection with intelligent machines.</h2>
+        </div>
+        <div className="mb-8 mt-8 flex relative top-60">
+          <Image src={"/background4.jpg"} alt={'lol'} width={900} height={800} />
         </div>
       </div>
       </div>
