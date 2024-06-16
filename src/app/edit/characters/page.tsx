cf7d69fd0f7c69427a6bb2cb8d40077a8c3bbcbe
chadeus <saze@protonmail.com>
2024-06-16 15:59:37 +1000

    styling

--- src/app/edit/characters/page.tsx
+++ src/app/edit/characters/page.tsx
@@ -70,7 +70,7 @@ const CharacterListPage = ({}) => {
             </Button>
           </a>
           </div>
-          <div className="flex absolute top-20 right-0 w-full">
+          <div className="absolute top-20 right-0 w-full">
             {characters.map((character) => (
               <ListItem
                 key={character.id}
